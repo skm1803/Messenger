87451abd9a56108447d189cc09868e8a7169d210
Shubham Mishra <shubham.mishrakiit@gmail.com>
2024-09-14 18:16:16 +0530

    implement design for the register page and login page also created the route

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function Login() {
+    return (
+        <div>
+            <Link to="/mess">Login</Link>
+        </div>
+    )
+}
+
+export default Login
