f0e297eb0f8bd34cfc5f08eb71c6ee97113776f1
Shubham Mishra <shubham.mishrakiit@gmail.com>
2024-09-15 14:26:10 +0530

    Implement state on register page

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Register() {
+    const [state, setstate] = useState({
+        userName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        image: ''
+    })
+
+    const inputHendle = (e) => {
+        setstate({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
     return (
         <div className='register'>
             <div className='card'>
@@ -12,19 +27,19 @@ function Register() {
                     <form>
                         <div className='form-group'>
                             <label htmlFor='username'>User Name</label>
-                            <input type="text" className='form-control' placeholder='User Name' id='username' />
+                            <input type="text" onChange={inputHendle} name="userName" value={state.userName} className='form-control' placeholder='User Name' id='username' />
                         </div>
                         <div className='form-group'>
                             <label htmlFor='email'>Email</label>
-                            <input type="email" className='form-control' placeholder='Email' id='email' />
+                            <input type="email" onChange={inputHendle} name="email" value={state.email} className='form-control' placeholder='Email' id='email' />
                         </div>
                         <div className='form-group'>
                             <label htmlFor='password'>Password</label>
-                            <input type="password" className='form-control' placeholder='Password' id='password' />
+                            <input type="password" onChange={inputHendle} name="password" value={state.password} className='form-control' placeholder='Password' id='password' />
                         </div>
                         <div className='form-group'>
                             <label htmlFor='confirmPassword'>Confirm Password</label>
-                            <input type="password" className='form-control' placeholder='Confirm Password' id='confirmPassword' />
+                            <input type="password" onChange={inputHendle} name="confirmPassword" value={state.confirmPassword} className='form-control' placeholder='Confirm Password' id='confirmPassword' />
                         </div>
                         <div className='form-group'>
                             <div className='file-image'>
